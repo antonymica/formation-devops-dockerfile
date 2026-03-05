40711e7d9af33075bfad71a6a56dfbedeaa46709
Micael Tahintsoa <mtahintsoa@gmail.com>
2026-03-05 11:48:06 +0300

    feat to do app

---
+++ src/TodoItem.tsx
@@ -0,0 +1,55 @@
+import { Trash } from 'lucide-react';
+
+type Priority = 'Urgente' | 'Moyenne' | 'Basse';
+
+type Todo = {
+  id: number;
+  text: string;
+  priority: Priority;
+};
+
+type Props = {
+  todo: Todo;
+  onDelete: () => void;
+  isSelected: boolean;
+  onToggleSelect: (id: number) => void;
+};
+
+const TodoItem = ({ todo, onDelete, isSelected, onToggleSelect }: Props) => {
+  return (
+    <li className="p-3">
+      <div className="flex justify-between items-center">
+        <div className="flex items-center gap-2">
+          <input
+            type="checkbox"
+            checked={isSelected}
+            className="checkbox checkbox-primary checkbox-sm"
+            onChange={() => onToggleSelect(todo.id)}
+          />
+          <span className="text-md font-bold">
+            <span>{todo.text}</span>
+          </span>
+          <span
+            className={`
+                badge badge-sm badge-soft
+                ${
+                  todo.priority === 'Urgente'
+                    ? 'badge-error'
+                    : todo.priority === 'Moyenne'
+                      ? 'badge-warning'
+                      : 'badge-success'
+                }
+            `}
+          >
+            {todo.priority}
+          </span>
+        </div>
+        <button className="btn btn-error btn-soft " onClick={onDelete}>
+          <Trash className="w-4 h-4" />
+        </button>
+      </div>
+    </li>
+  );
+};
+
+export default TodoItem;
